911fbe63145b89cb3df85588ec8e4a024246e346
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2022-10-18 09:00:15 -0400

    Add login & delete account

--- prisma/src/index.ts
+++ prisma/src/index.ts
@@ -103,14 +103,12 @@ app.post("/api/auth/signup", async (req: { body: User }, res) => {
 
     // Generate a token for the user to use in the future
     const token = jwt.sign(
-      { 
-        username: user.username, 
-        email: user.email 
+      {
+        username: user.username,
+        email: user.email,
       },
       process.env.JWT_SECRET,
-      { expiresIn: 
-        "12h" 
-      }
+      { expiresIn: "12h" }
     );
 
     console.log("Token generated: ", token);
@@ -130,6 +128,93 @@ app.post("/api/auth/signup", async (req: { body: User }, res) => {
         return;
       }
     }
+    console.log("Unknown error:" + error);
+    res.sendStatus(500);
+    return;
+  }
+});
+
+interface LoginCredentials {
+  email: string;
+  password: string;
+}
+
+// Login a user
+// http://localhost:3000/api/auth/login
+// don't forget to attach the JSON body to the request
+app.post(
+  "/api/auth/login",
+  async (req: { body: { email: string; password: string } }, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.sendStatus(400);
+      console.log("Missing required fields.");
+      return;
+    }
+    try {
+      const user = await prisma.users.findUnique({
+        where: {
+          email: String(email),
+        },
+      });
+      if (user) {
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (isPasswordCorrect) {
+          const token = jwt.sign(
+            {
+              username: user.username,
+              email: user.email,
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: "12h" }
+          );
+          const result = { ...user, token };
+          res.status(200).json(result);
+        } else {
+          res.sendStatus(401);
+        }
+      } else {
+        res.sendStatus(401);
+      }
+    } catch (error) {
+      console.log("Unknown error:" + error);
+      res.sendStatus(500);
+      return;
+    }
+  }
+);
+
+app.post('/api/account/delete', async (req, res) => {
+
+  const token = req.headers["authorization"]?.slice(7);
+
+  console.log(token);
+
+  
+  let username = "";
+
+  if (!token) {
+    return res.status(401).send({ auth: false, message: "No token provided." });
+  }
+
+  await jwt.verify(token, process.env.JWT_SECRET, function (err: any, decoded: any) {
+    if (err) {
+      return res
+        .status(500)
+        .send({ auth: false, message: "Failed to authenticate token." });
+    }
+    // if everything good, save to request for use in other routes
+    username = decoded.username;
+  });
+
+  try {
+    const result = await prisma.users.delete({
+      where: {
+        username: username,
+      },
+    });
+    res.status(200).json(result);
+  } catch (error) {
     console.log("Unknown error:" + error);
     res.sendStatus(500);
     return;
