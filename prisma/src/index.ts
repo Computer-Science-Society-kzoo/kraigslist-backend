ff1d370b3c9300e54e7848a5910325eb35ede6ec
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2022-10-17 21:39:03 -0400

    add sign up API

--- prisma/src/index.ts
+++ prisma/src/index.ts
@@ -1,149 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient, users } from "@prisma/client";
 import express from "express";
 
 const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 
-// app.post(`/signup`, async (req, res) => {
-//   const { name, email, posts } = req.body
 
-//   const postData = posts?.map((post: Prisma.PostCreateInput) => {
-//     return { title: post?.title, content: post?.content }
-//   })
-
-//   const result = await prisma.user.create({
-//     data: {
-//       name,
-//       email,
-//       posts: {
-//         create: postData,
-//       },
-//     },
-//   })
-//   res.json(result)
-// })
-
-// app.post(`/post`, async (req, res) => {
-//   const { title, content, authorEmail } = req.body
-//   const result = await prisma.post.create({
-//     data: {
-//       title,
-//       content,
-//       author: { connect: { email: authorEmail } },
-//     },
-//   })
-//   res.json(result)
-// })
-
-// app.put('/post/:id/views', async (req, res) => {
-//   const { id } = req.params
-
-//   try {
-//     const post = await prisma.post.update({
-//       where: { id: Number(id) },
-//       data: {
-//         viewCount: {
-//           increment: 1,
-//         },
-//       },
-//     })
-
-//     res.json(post)
-//   } catch (error) {
-//     res.json({ error: `Post with ID ${id} does not exist in the database` })
-//   }
-// })
-
-// app.put('/publish/:id', async (req, res) => {
-//   const { id } = req.params
-
-//   try {
-//     const postData = await prisma.post.findUnique({
-//       where: { id: Number(id) },
-//       select: {
-//         published: true,
-//       },
-//     })
-
-//     const updatedPost = await prisma.post.update({
-//       where: { id: Number(id) || undefined },
-//       data: { published: !postData?.published },
-//     })
-//     res.json(updatedPost)
-//   } catch (error) {
-//     res.json({ error: `Post with ID ${id} does not exist in the database` })
-//   }
-// })
-
-// app.delete(`/post/:id`, async (req, res) => {
-//   const { id } = req.params
-//   const post = await prisma.post.delete({
-//     where: {
-//       id: Number(id),
-//     },
-//   })
-//   res.json(post)
-// })
-
-// app.get('/users', async (req, res) => {
-//   const users = await prisma.user.findMany()
-//   res.json(users)
-// })
-
-// app.get('/user/:id/drafts', async (req, res) => {
-//   const { id } = req.params
-
-//   const drafts = await prisma.user
-//     .findUnique({
-//       where: {
-//         id: Number(id),
-//       },
-//     })
-//     .posts({
-//       where: { published: false },
-//     })
-
-//   res.json(drafts)
-// })
-
-// app.get(`/post/:id`, async (req, res) => {
-//   const { id }: { id?: string } = req.params
-
-//   const post = await prisma.post.findUnique({
-//     where: { id: Number(id) },
-//   })
-//   res.json(post)
-// })
-
-// app.get('/feed', async (req, res) => {
-//   const { searchString, skip, take, orderBy } = req.query
-
-//   const or: Prisma.PostWhereInput = searchString
-//     ? {
-//         OR: [
-//           { title: { contains: searchString as string } },
-//           { content: { contains: searchString as string } },
-//         ],
-//       }
-//     : {}
-
-//   const posts = await prisma.post.findMany({
-//     where: {
-//       published: true,
-//       ...or,
-//     },
-//     include: { author: true },
-//     take: Number(take) || undefined,
-//     skip: Number(skip) || undefined,
-//     orderBy: {
-//       updatedAt: orderBy as Prisma.SortOrder,
-//     },
-//   })
-
-//   res.json(posts)
-// })
-
+// Start the server
 const server = app.listen(3000, () => {
   const message = `
     🚀 Server ready at: http://localhost:3000
@@ -152,16 +15,21 @@ const server = app.listen(3000, () => {
   console.log(message);
 });
 
+// Get response from the server's main page
 app.get("/", async (req, res) => {
   const message = "Welcome to the the Kraigslist Backend";
   res.send(message);
 });
 
+
+// Get all users in a JSON format
 app.get("/users", async (req, res) => {
   const users = await prisma.users.findMany();
   res.json(users);
 })
 
+// Get a specific user by their username
+// http://localhost:3000/user/username?=aleksandr
 app.get("/user", async (req, res) => {
   const username = req.query.username;
   console.log("Looking for user: ", username);
@@ -174,3 +42,60 @@ app.get("/user", async (req, res) => {
   res.json(user);
 })
 
+
+interface User {
+  username: string;
+  email: string;
+  password: string;
+  first_name: string;
+  last_name: string;
+  preferred_name: string;
+  year: number;
+}
+
+// Create a new user
+// http://localhost:3000/api/auth/signup
+// don't forget to attach the JSON body to the request 
+app.post('/api/auth/signup', async (req: {body: User}, res) => {
+  // Getting the user from the body in a JSON format
+  const { username, email, password, first_name, last_name, preferred_name, year } = req.body;
+  // If fields are missing, return an error
+  if (!username || !email || !password || !first_name || !last_name || !year) {
+    res.sendStatus(400);
+    console.log("Missing required fields.");
+    return;
+  }
+  // Try block is important if we want to avoid crashes and catch errors
+  // If server crashes, we have to restart it manually :/
+  try {
+    //Create a new user in the database
+    const result = await prisma.users.create({
+      data: {
+        username,
+        email,
+        password,
+        first_name,
+        last_name,
+        preferred_name,
+        year
+    }});
+    console.log('New user is created: ', result);
+    // Send a success response
+    res.sendStatus(200);
+  } catch (error) {
+    // If the error is known...
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      // If the error is a unique constraint violation... (username exists)
+      if (error.code === 'P2002') {
+        console.log(
+          'There is a unique constraint violation, a new user cannot be created with this username.'
+        )
+        res.sendStatus(409);
+        return
+      }
+    }
+    console.log("Unknown error:" + error);
+    res.sendStatus(500);
+    return
+  }
+});
