f71b87fca4a2b6909ca76b41c96a22e95be5e973
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2022-10-18 07:26:20 -0400

    Secure sign up process + add comments

--- prisma/src/index.ts
+++ prisma/src/index.ts
@@ -1,10 +1,21 @@
+// Prisma is a modern DB toolkit to query, migrate and model your database
+// It automatically generates and maintains the database schema for us.
+// https://www.prisma.io/
 import { Prisma, PrismaClient, users } from "@prisma/client";
-import express from "express";
-
 const prisma = new PrismaClient();
+
+// Express is a web framework for Node.js that let us build APIs
+// https://www.npmjs.com/package/express
+import express from "express";
 const app = express();
 app.use(express.json());
 
+// Bcrypt is a library that let us hash passwords before we store them in the database
+// https://www.npmjs.com/package/bcrypt
+const bcrypt = require("bcrypt");
+const saltRounds = 10; // The higher the number, the more secure the password
+
+var jwt = require("jsonwebtoken");
 
 // Start the server
 const server = app.listen(3000, () => {
@@ -21,12 +32,11 @@ app.get("/", async (req, res) => {
   res.send(message);
 });
 
-
 // Get all users in a JSON format
 app.get("/users", async (req, res) => {
   const users = await prisma.users.findMany();
   res.json(users);
-})
+});
 
 // Get a specific user by their username
 // http://localhost:3000/user/username?=aleksandr
@@ -40,8 +50,7 @@ app.get("/user", async (req, res) => {
   });
   console.log("User found. Sending the response back.");
   res.json(user);
-})
-
+});
 
 interface User {
   username: string;
@@ -55,10 +64,18 @@ interface User {
 
 // Create a new user
 // http://localhost:3000/api/auth/signup
-// don't forget to attach the JSON body to the request 
-app.post('/api/auth/signup', async (req: {body: User}, res) => {
+// don't forget to attach the JSON body to the request
+app.post("/api/auth/signup", async (req: { body: User }, res) => {
   // Getting the user from the body in a JSON format
-  const { username, email, password, first_name, last_name, preferred_name, year } = req.body;
+  const {
+    username,
+    email,
+    password,
+    first_name,
+    last_name,
+    preferred_name,
+    year,
+  } = req.body;
   // If fields are missing, return an error
   if (!username || !email || !password || !first_name || !last_name || !year) {
     res.sendStatus(400);
@@ -68,34 +85,53 @@ app.post('/api/auth/signup', async (req: {body: User}, res) => {
   // Try block is important if we want to avoid crashes and catch errors
   // If server crashes, we have to restart it manually :/
   try {
+    // Hash the password before storing it in the database (later use bscrypt.compare to check the password)
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
     //Create a new user in the database
-    const result = await prisma.users.create({
+    const user = await prisma.users.create({
       data: {
         username,
         email,
-        password,
+        password: hashedPassword,
         first_name,
         last_name,
         preferred_name,
-        year
-    }});
-    console.log('New user is created: ', result);
+        year,
+      },
+    });
+    console.log("New user is created: ", user);
+
+    // Generate a token for the user to use in the future
+    const token = jwt.sign(
+      { 
+        username: user.username, 
+        email: user.email 
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: 
+        "12h" 
+      }
+    );
+
+    console.log("Token generated: ", token);
+    // Save the result
+    const result = { ...user, token };
     // Send a success response
-    res.sendStatus(200);
+    res.status(200).json(result);
   } catch (error) {
     // If the error is known...
     if (error instanceof Prisma.PrismaClientKnownRequestError) {
       // If the error is a unique constraint violation... (username exists)
-      if (error.code === 'P2002') {
+      if (error.code === "P2002") {
         console.log(
-          'There is a unique constraint violation, a new user cannot be created with this username.'
-        )
+          "There is a unique constraint violation, a new user cannot be created with this username."
+        );
         res.sendStatus(409);
-        return
+        return;
       }
     }
     console.log("Unknown error:" + error);
     res.sendStatus(500);
-    return
+    return;
   }
 });
