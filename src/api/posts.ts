a2cc7fcce593493c90767cab53738bc070e42486
Shanonbrown <brownshanonalisha@gmail.com>
2022-10-31 20:40:32 -0400

    Revert "Merge branch 'main' of https://github.com/Computer-Science-Society-kzoo/kraigslist-backend"
    This reverts commit 82aa9d7b007cf7fe0bb1b11e47e86402c47d3d16, reversing
    changes made to 4fca88df5aa377e0fc1848c3d2a6810ccaf01ad2.

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -28,20 +28,14 @@ app.use(express.json());
 
 import { userIsLoggedIn } from "./auth";
 
-// Get all Posts in a JSON format by most recent
+// Get all Posts in a JSON format
 export const getPosts = async (req: any, res: any) => {
-  console.log(req.cookies); 
-  if (req.cookies.auth === undefined) { 
-    res.status(401); 
+  console.log(req.cookies);
+  if (req.cookies.auth === undefined) {
+    res.status(401);
   } else if (userIsLoggedIn(req.cookies.auth)) {
-    const posts = await prisma.posts.findMany({
-      orderBy: [
-        {
-          dt_created: 'desc',
-        },
-      ],}
-    ); 
-    res.json(posts); 
+    const posts = await prisma.posts.findMany();
+    res.json(posts);
   }
   res.status(401);
 };
@@ -61,8 +55,6 @@ interface Post2 {
   text: string;
   type: string;
   category: string;
-  offer_deadline: any;
-  price: number;
   img: any; // "any" might be the wrong move here, but byte or any version of that didnt seem to work.
 }
 
@@ -99,7 +91,7 @@ export const createPost = async (
     return;
   }
   const dt_created = new Date();
-  const { title, text, type, category, offer_deadline, price, img } = req.body;
+  const { title, text, type, category, img } = req.body;
   if (!title || !text || !username || !type || !category) {
     res.sendStatus(400);
     console.log("Missing required fields like title, text, type, or category.");
@@ -125,8 +117,6 @@ export const createPost = async (
           username,
           type,
           category,
-          offer_deadline,
-          price,
           img,
           userID
         },
@@ -164,9 +154,11 @@ export const getMyPosts = async (req: string, res: any) => {
   }
 };
 
-//make a request to find posts with either a title or text that contains the search term
+//Tabitha's work in progress for search bar
+
 //type GET
 export const searchPosts = async (toSearch: string, res: any) => {
+//make a request to find posts with either a title or text that contains the search term
 try { 
     const result = await prisma.posts.findMany({
         where: {
@@ -184,3 +176,10 @@ try {
     return;
 } 
 };
+ 
+//Shanon's work in progress for deleting posts if:
+// user is authentic [is logged in] + is the same user who created the post
+// first: check to see if the user is authenticated ...
+// ALSO: you can't delete a post if it's not created
+// LOOK AT: CreatePost function for reference
+
